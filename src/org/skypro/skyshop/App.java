8ef34d83ef9759d22c742e9d8f3d08bdb52aa7e4
Alexandra <saa-saa@mail.ru>
2025-05-15 00:47:44 +0300

    Домашнее_задание

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,17 +1,24 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.Product;
 
 public class App {
     public static void main(String[] args) {
         ProductBasket pul = new ProductBasket();
-        pul.addToBasket("Книга", 2500);
-        pul.addToBasket("Ручка синяя", 85);
-        pul.addToBasket("Клей", 110);
-        pul.addToBasket("Набор фломастеров", 320);
-        pul.addToBasket("Тетрадь", 30);
+        Product book=new Product("Книга", 2500);
+        Product pencil = new Product("Ручка синяя", 85);
+        Product clue = new Product("Клей", 110);
+        Product markerSet = new Product("Набор фломастеров", 320);
+        Product copyBook= new Product("Тетрадь", 30);
+        Product beer = new Product("Пиво", 90);
+        pul.addToBasket(book);
+        pul.addToBasket(pencil);
+        pul.addToBasket(clue);
+        pul.addToBasket(markerSet);
+        pul.addToBasket(copyBook);
         pul.showTotalBasket();
-        pul.addToBasket("Пиво", 90);
+        pul.addToBasket(beer);
         pul.findProduct("Тетрадь");
         pul.findProduct("Бумага цветная");
         pul.cleanBasket();
--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,5 +1,7 @@
 package org.skypro.skyshop.basket;
+
 import org.skypro.skyshop.product.Product;
+
 import java.util.Arrays;
 
 public class ProductBasket {
@@ -8,16 +10,16 @@ public class ProductBasket {
 
     public ProductBasket() {
         this.products = new Product[5];
-        size=0;
+        size = 0;
     }
 
-    public void addToBasket(String productName, int productPrice) {
-        if (size<products.length){
-            products[size]= new Product(productName,productPrice);
+    public void addToBasket(Product product) {
+        if (size < products.length) {
+            products[size] = product;
             size++;
-        }
-        if (size>=products.length){
-            System.out.println("Невозможно добавить продукт");
+            System.out.println("Продукт "+product.getProductName()+" успешно добавлен в корзину");
+        } else {
+            System.out.println("Невозможно добавить продукт "+product.getProductName()+". Корзина переполнена.");
         }
     }
 
@@ -37,25 +39,27 @@ public class ProductBasket {
         } else {
             for (Product product : products) {
                 if (product != null) {
-                    System.out.println(product.getProductName() + ": " + product.getProductPrice()+" р.");
+                    System.out.println(product.getProductName() + ": " + product.getProductPrice() + " р.");
                 }
 
-            }System.out.println("Итоговая сумма покупок: "+countTotalPrice()+" р.");
+            }
+            System.out.println("Итоговая сумма покупок: " + countTotalPrice() + " р.");
         }
     }
 
-    public boolean findProduct(String productName){
+    public boolean findProduct(String productName) {
         for (Product product : products) {
-            if((product!=null)&&(productName.equals(product.getProductName()))){
+            if ((product != null) && (productName.equals(product.getProductName()))) {
                 System.out.println("У вас уже есть такой товар");
                 return true;
             }
-        }System.out.println("Такого товара нет");
+        }
+        System.out.println("Такого товара нет");
         return false;
     }
 
 
-    public void cleanBasket(){
+    public void cleanBasket() {
         Arrays.fill(products, null);
         System.out.println("Корзина пуста");
     }
