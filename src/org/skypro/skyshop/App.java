bcd6d90792a483bb4abc70badcc88994a7970e34
Alexandra <saa-saa@mail.ru>
2025-05-29 00:53:21 +0300

    Домашнее_задание_интерфейсы

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -11,8 +11,10 @@ public class App {
         SimpleProduct clue = new SimpleProduct("Клей", 110);
         DiscountedProduct markerSet = new DiscountedProduct("Набор фломастеров", 320,10);
         DiscountedProduct copyBook= new DiscountedProduct("Тетрадь", 30,5);
-        Article livro = new Article("Крепость","Книга о поиске себя");
+        Article livroFortress = new Article("Крепость","Книга о поиске себя");
+        Article livroBridge = new Article("Мост на Дрине","Книга о вечности и людских судьбах");
         Article cola = new Article("Клей канцелярский","Мощный клей - даже в воде не растворяется");
+        Article superCola = new Article("Клей момент","В два раза больше мощности!");
 //        pul.addToBasket(book);
 //        pul.addToBasket(pencil);
 //        pul.addToBasket(clue);
@@ -32,11 +34,14 @@ public class App {
         wishList.addToSearchable(clue);
         wishList.addToSearchable(markerSet);
         wishList.addToSearchable(copyBook);
-        wishList.addToSearchable(livro);
+        wishList.addToSearchable(livroFortress);
+        wishList.addToSearchable(livroBridge);
         wishList.addToSearchable(cola);
+        wishList.addToSearchable(superCola);
+
 
         wishList.showSearchable();//Проверяю все ли добавлено в массив
-        String keyword = "и";
+        String keyword = "книга";
         wishList.searchByKeyword(keyword);
     }
 }
